4550116edcc829c6d743dc20e71326e1e6599f4a
k-joseph <kaweesi@jembi.org>
2016-07-21 12:47:02 +0300

    RWSPU-32: invoking authentication on 1.9.x downwards using configured credentials

--- api/src/main/java/org/openmrs/module/systemmonitor/scheduler/PushMonitoredDataTask.java
+++ api/src/main/java/org/openmrs/module/systemmonitor/scheduler/PushMonitoredDataTask.java
@@ -1,6 +1,10 @@
 package org.openmrs.module.systemmonitor.scheduler;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.openmrs.api.context.Context;
+import org.openmrs.module.ModuleUtil;
+import org.openmrs.module.systemmonitor.SystemMonitorConstants;
 import org.openmrs.module.systemmonitor.api.SystemMonitorService;
 import org.openmrs.scheduler.tasks.AbstractTask;
 
@@ -14,8 +18,19 @@ import org.openmrs.scheduler.tasks.AbstractTask;
  */
 public class PushMonitoredDataTask extends AbstractTask {
 
+	private Log log = LogFactory.getLog(getClass());
+
+	@SuppressWarnings("deprecation")
 	@Override
 	public void execute() {
+		try {
+			if (!ModuleUtil.matchRequiredVersions(SystemMonitorConstants.OPENMRS_VERSION, "1.9")
+					&& !Context.isAuthenticated()) {
+				authenticate();
+			}
+		} catch (Exception e) {
+			log.error(e);
+		}
 		Context.getService(SystemMonitorService.class).pushMonitoredDataToDHIS();
 	}
 
--- api/src/main/java/org/openmrs/module/systemmonitor/scheduler/UpdateLocallyStoredDHISMetadataFromRemoteTask.java
+++ api/src/main/java/org/openmrs/module/systemmonitor/scheduler/UpdateLocallyStoredDHISMetadataFromRemoteTask.java
@@ -1,21 +1,36 @@
 package org.openmrs.module.systemmonitor.scheduler;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.openmrs.api.context.Context;
+import org.openmrs.module.ModuleUtil;
+import org.openmrs.module.systemmonitor.SystemMonitorConstants;
 import org.openmrs.module.systemmonitor.api.SystemMonitorService;
 import org.openmrs.scheduler.tasks.AbstractTask;
 
 /**
  * This Task sends data to a configured remote task and is represented by
- * scheduler_task_config entry named: 'Update Locally Stored DHIS Metadata' meant
- * to run once a day
+ * scheduler_task_config entry named: 'Update Locally Stored DHIS Metadata'
+ * meant to run once a day
  * 
  * @author k-joseph
  *
  */
 public class UpdateLocallyStoredDHISMetadataFromRemoteTask extends AbstractTask {
 
+	private Log log = LogFactory.getLog(getClass());
+
+	@SuppressWarnings("deprecation")
 	@Override
 	public void execute() {
+		try {
+			if (!ModuleUtil.matchRequiredVersions(SystemMonitorConstants.OPENMRS_VERSION, "1.9")
+					&& !Context.isAuthenticated()) {
+				authenticate();
+			}
+		} catch (Exception e) {
+			log.error(e);
+		}
 		Context.getService(SystemMonitorService.class).updateLocallyStoredDHISMetadata();
 	}
 
