8f633343bd8710e7ac7e1577163b389fa8b7490d
k-joseph <kaweesi@jembi.org>
2017-02-08 12:38:30 +0300

    preparing to release 1.5

--- api/src/main/java/org/openmrs/module/systemmonitor/scheduler/MemoryAggregationTask.java
+++ api/src/main/java/org/openmrs/module/systemmonitor/scheduler/MemoryAggregationTask.java
@@ -2,7 +2,6 @@ package org.openmrs.module.systemmonitor.scheduler;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.openmrs.api.context.Context;
 import org.openmrs.module.systemmonitor.memory.MemoryAggregation;
 import org.openmrs.scheduler.tasks.AbstractTask;
 
